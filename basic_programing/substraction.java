636f134d93194f96a6422525945a429533fee1bc
Abhishek Bhargava <abhishekbhargava@Abhisheks-MacBook-Air.local>
2026-01-19 18:41:14 +0530

    subtraction file

---
+++ basic_programing/substraction.java
@@ -0,0 +1,14 @@
+package basic_programing;
+
+import java.util.Scanner;
+
+public class substraction {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter two number");
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int c=a-b;
+		System.out.println("Difference of two number is :-" +c);
+	}
+}
