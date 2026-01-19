8310c37f579e27be514d5a8d2014d90f28926bb2
Abhishek Bhargava <abhishekbhargava@Abhisheks-MacBook-Air.local>
2026-01-19 18:34:10 +0530

    addition file

---
+++ basic_programing/addition.java
@@ -0,0 +1,18 @@
+package basic_programing;
+
+import java.util.Scanner;
+
+public class addition {
+
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter two number");
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int c=a+b;
+		System.out.println("Sum of two number is :-" +c);
+		
+	}
+
+}
