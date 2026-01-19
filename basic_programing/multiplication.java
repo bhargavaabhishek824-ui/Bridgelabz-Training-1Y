a3095c3610b8f47ae3477a393f11181764ddb536
Abhishek Bhargava <abhishekbhargava@Abhisheks-MacBook-Air.local>
2026-01-19 18:50:43 +0530

    multiplication file

---
+++ basic_programing/multiplication.java
@@ -0,0 +1,16 @@
+package basic_programing;
+
+import java.util.Scanner;
+
+public class multiplication {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter two number");
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int c=a*b;
+		System.out.println("Multiplication of two number is :-" +c);
+	}
+
+}
