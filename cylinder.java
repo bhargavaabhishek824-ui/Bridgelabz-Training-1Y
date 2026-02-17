f4ed78648a8ae6b2169996620506e8397663dd27
Abhishek Bhargava <abhishekbhargava@Abhisheks-MacBook-Air.local>
2026-02-17 23:42:30 +0530

    basic_programing

---
+++ cylinder.java
@@ -0,0 +1,12 @@
+package basic_programing;
+import java.util.*;
+public class cylinder {
+
+	public static void main(String[] args) {
+		int r=2;
+		int h=4;
+		double volume=3.14*r*r*h;
+		System.out.println("volume of cylinder is: - "+volume);
+	}
+
+}
---
+++ samsulek.java
@@ -0,0 +1,19 @@
+package basic_programing;
+import java.util.*;
+public class samsulek {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("maths marks:");
+		int m=sc.nextInt();
+		System.out.println("physics marks:");
+		int p=sc.nextInt();
+		System.out.println("chemistry marks:");
+		int c=sc.nextInt();
+		int Average=(m+p+c)/3;
+		System.out.println("Average:"+Average);
+		
+
+	}
+
+}
