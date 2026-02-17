d76d7dd03ec114d4b37c423bf71763090877b962
Abhishek Bhargava <abhishekbhargava@Abhisheks-MacBook-Air.local>
2026-02-17 22:39:52 +0530

    Basic Coding Problems

---
+++ circle.java
@@ -0,0 +1,14 @@
+package basic_programing;
+import java.util.*;
+public class circle {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter radius");
+		int r=sc.nextInt();
+		double area=2*3.14*r;
+		System.out.println("area is = "+area);
+
+	}
+
+}
---
+++ volume_of_earth.java
@@ -0,0 +1,14 @@
+package basic_programing;
+import java.util.*;
+public class volume_of_earth {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Radius of earth");
+		int radius=sc.nextInt();
+		double volume=(4.0/3.0)*3.14*radius*radius*radius;
+	System.out.println("vouume of earth"+volume);
+
+	}
+
+}
