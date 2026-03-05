ddc016343743709f884729457a521b347f3411aa
bhargavaabhishek824-ui <bhargavaabhishek824@gmail.com>
2026-03-06 00:41:09 +0530

    Delete com/gla directory

--- com/gla/compare_string.java
+++
@@ -1,9 +0,0 @@
-package com.gla;
-import java.util.*;
-public class compare_string {
-public static void main(String[] args) {
-	Scanner sc=new Scanner(System.in);
-	String a=sc.nextLine();
-	
-}
-}
