dbde5c0b5f54c14703d892895f41dc9ab86b2221
bhargavaabhishek824-ui <bhargavaabhishek824@gmail.com>
2026-03-22 12:06:28 +0530

    Delete Inheritance directory

--- Inheritance/Animals/Bird.java
+++
@@ -1,9 +0,0 @@
-package Animals;
-public class Bird extends Animal{
-    public Bird(String name,int age){
-        super(name,age);
-    }
-    public void makeSound(){
-        System.out.println(name+": Chi Chi Chu Chu");
-    }
-}
--- Inheritance/Animals/Dog.java
+++
@@ -1,9 +0,0 @@
-package Animals;
-public class Dog extends Animal{
-    public Dog(String name,int age){
-        super(name,age);
-    }
-    public void makeSound(){
-        System.out.println(name+": Bhau Bhau ");
-    }
-}
