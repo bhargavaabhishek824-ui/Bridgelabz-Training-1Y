abfadc920828daf4da24f7f33fa02ee944467319
bhargavaabhishek824-ui <bhargavaabhishek824@gmail.com>
2026-03-29 09:37:08 +0530

    Delete Inheritance directory

--- Inheritance/Animals/Cat.java
+++
@@ -1,9 +0,0 @@
-package Animals;
-public class Cat extends Animal{
-    public Cat(String name,int age) {
-        super(name, age);
-    }
-    public void makeSound(){
-        System.out.println(name+": Meowwwwwwwwww ");
-    }
-}
