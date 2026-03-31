638c78f2e4395b1942bf3729afd0f5eeb45ce76f
Abhishek Bhargava <abhishekbhargava@Abhisheks-MacBook-Air-2.local>
2026-03-31 12:43:52 +0530

    MultiThreadingTwo

---
+++ MultiThreadingTwo/Runner.java
@@ -0,0 +1,10 @@
+package MultiThreadingTwo;
+public class Runner {
+    static void main(String[] args) {
+        Thread1 t1 = new Thread1();
+        Thread2 t2 = new Thread2();
+        Thread t = new Thread();
+        t1.start();
+        t1.start();
+    }
+}
