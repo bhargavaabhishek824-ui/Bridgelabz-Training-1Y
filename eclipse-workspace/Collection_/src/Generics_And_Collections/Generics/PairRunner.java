5ed48c190290f347d97d88f30156b85bdd7de36c
bhargavaabhishek824-ui <bhargavaabhishek824@gmail.com>
2026-04-25 23:03:26 +0530

    Delete eclipse-workspace/Collection_/src/Generics_And_Collections directory

--- eclipse-workspace/Collection_/src/Generics_And_Collections/Generics/PairRunner.java
+++
@@ -1,8 +0,0 @@
-package Generics_And_Collections.Generics;
-public class PairRunner {
-        public static void main(String[] args){
-            Pair<String,Integer> student=new Pair<>("Ankush",19);
-            System.out.println("Name: "+student.getFirst());
-            System.out.println("Age: "+student.getSecond());
-        }
-}
